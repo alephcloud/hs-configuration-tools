157ecf41f7a2583a04beed1ccb8432099e5d0f72
Lars Kuhtz <lars@alephcloud.com>
2014-11-08 02:15:04 -0800

    update Setup.hs

--- Setup.hs
+++ Setup.hs
@@ -26,7 +26,7 @@
 --
 -- = Usage as Setup Script
 --
--- There are two ways how this module can be used:
+-- There are three ways how this module can be used:
 --
 -- 1. Copy the code of this module into a file called @Setup.hs@ in the root
 --    directory of your package.
@@ -38,7 +38,18 @@
 --    >
 --    > import Configuration.Utils.Setup
 --
--- With both methods the field @Build-Type@ in the package description (cabal) file
+-- 3. For usage within a more complex @Setup.hs@ script you shall import this
+--    module qualified and use the 'mkPkgInfoModules' function. For example:
+--
+--    > module Main (main) where
+--    >
+--    > import qualified Configuration.Utils.Setup as ConfTools
+--    >
+--    > main :: IO ()
+--    > main = defaultMainWithHooks (ConfTools.mkPkgInfoModules simpleUserHooks)
+--    >
+--
+-- With all methods the field @Build-Type@ in the package description (cabal) file
 -- must be set to @Custom@:
 --
 -- > Build-Type: Custom
@@ -90,7 +101,10 @@
 -- [@--license@]
 --     prints the text of the lincense of the application and exits.
 --
-module Main (main) where
+module Main
+( main
+, mkPkgInfoModules
+) where
 
 import Distribution.PackageDescription
 import Distribution.Simple
@@ -116,18 +130,52 @@ import Prelude hiding (readFile, writeFile)
 import System.Directory (doesFileExist, doesDirectoryExist, createDirectoryIfMissing)
 import System.Exit (ExitCode(ExitSuccess))
 
+-- | Include this function when your setup doesn't contain any
+-- extra functionality.
+--
 main :: IO ()
-main = defaultMainWithHooks simpleUserHooks
-    { postConf = mkPkgInfoModules
+main = defaultMainWithHooks (mkPkgInfoModules simpleUserHooks)
+
+-- | Modifies the given record of hooks by adding functionality that
+-- creates a package info module for each component of the cabal package.
+--
+-- This function is intended for usage in more complex @Setup.hs@ scripts.
+-- If your setup doesn't contain any other function you can just import
+-- the 'main' function from this module.
+--
+-- The modules are created in the /autogen/ build directory where also the
+-- @Path_@ module is created by cabal's simple build setup. This is usually the
+-- directory @.\/dist\/build\/autogen@.
+--
+-- For a library component the module is named just @PkgInfo@. For all other
+-- components the module is named @PkgInfo_COMPONENT_NAME@ where
+-- @COMPONENT_NAME@ is the name of the component with @-@ characters replaced by
+-- @_@.
+--
+mkPkgInfoModules
+    :: UserHooks
+    -> UserHooks
+mkPkgInfoModules hooks = hooks
+    { postConf = mkPkgInfoModulesPostConf (postConf hooks)
     }
+
+mkPkgInfoModulesPostConf
+    :: (Args -> ConfigFlags -> PackageDescription -> LocalBuildInfo -> IO ())
+    -> Args
+    -> ConfigFlags
+    -> PackageDescription
+    -> LocalBuildInfo
+    -> IO ()
+mkPkgInfoModulesPostConf hook args flags pkgDesc bInfo = do
+    mkModules
+    hook args flags pkgDesc bInfo
   where
-    mkPkgInfoModules _ _ pkgDesc bInfo = mapM_ f . map (\(a,_,_) -> a) $ componentsConfigs bInfo
-      where
-        f cname = case cname of
-            CLibName -> updatePkgInfoModule Nothing pkgDesc bInfo
-            CExeName s -> updatePkgInfoModule (Just s) pkgDesc bInfo
-            CTestName s -> updatePkgInfoModule (Just s) pkgDesc bInfo
-            CBenchName s -> updatePkgInfoModule (Just s) pkgDesc bInfo
+    mkModules = mapM_ (f . \(a,_,_) -> a) $ componentsConfigs bInfo
+    f cname = case cname of
+        CLibName -> updatePkgInfoModule Nothing pkgDesc bInfo
+        CExeName s -> updatePkgInfoModule (Just s) pkgDesc bInfo
+        CTestName s -> updatePkgInfoModule (Just s) pkgDesc bInfo
+        CBenchName s -> updatePkgInfoModule (Just s) pkgDesc bInfo
 
 pkgInfoModuleName :: Maybe String -> String
 pkgInfoModuleName Nothing = "PkgInfo"
@@ -144,12 +192,15 @@ trim = f . f
   where f = reverse . dropWhile isSpace
 
 getVCS :: IO (Maybe RepoType)
-getVCS = do
+getVCS =
     doesDirectoryExist ".hg" >>= \x0 -> if x0
     then return (Just Mercurial)
-    else doesDirectoryExist ".git" >>= \x1 -> if x1
-    then return (Just Git)
-    else return Nothing
+    else doesDirectoryExist ".git" >>= \x1 -> return $ if x1
+        then Just Git
+        else Nothing
+
+flagNameStr :: FlagName -> String
+flagNameStr (FlagName s) = s
 
 pkgInfoModule :: Maybe String -> PackageDescription -> LocalBuildInfo -> IO B.ByteString
 pkgInfoModule cName pkgDesc bInfo = do
@@ -160,11 +211,11 @@ pkgInfoModule cName pkgDesc bInfo = do
 
     let vcsBranch = if branch == "default" || branch == "master" then "" else branch
         vcsVersion = intercalate "-" . filter (/= "") $ [tag, revision, vcsBranch]
-        flags = map fst . filter snd . configConfigurationsFlags . configFlags $ bInfo
+        flags = map (flagNameStr . fst) . filter snd . configConfigurationsFlags . configFlags $ bInfo
 
     licenseString <- licenseFilesText pkgDesc
 
-    return $ B.intercalate "\n" $
+    return $ B.intercalate "\n"
             [ "{-# LANGUAGE OverloadedStrings #-}"
             , "{-# LANGUAGE RankNTypes #-}"
             , ""
@@ -281,20 +332,20 @@ licenseFilesText PackageDescription{ licenseFiles = fileNames } =
 
 hgInfo :: IO (String, String, String)
 hgInfo = do
-    tag <- fmap trim $ readProcess "hg" ["id", "-r", "max(ancestors(\".\") and tag())", "-t"] ""
-    rev <- fmap trim $ readProcess "hg" ["id", "-i"] ""
-    branch <- fmap trim $ readProcess "hg" ["id", "-b"] ""
+    tag <- trim <$> readProcess "hg" ["id", "-r", "max(ancestors(\".\") and tag())", "-t"] ""
+    rev <- trim <$> readProcess "hg" ["id", "-i"] ""
+    branch <- trim <$> readProcess "hg" ["id", "-b"] ""
     return (tag, rev, branch)
 
 gitInfo :: IO (String, String, String)
 gitInfo = do
     tag <- do
-        (exitCode, out, _err) <- readProcessWithExitCode "git" ["describe", "--exact-match", "--abbrev=0"] ""
+        (exitCode, out, _err) <- readProcessWithExitCode "git" ["describe", "--exact-match", "--tags", "--abbrev=0"] ""
         case exitCode of
             ExitSuccess -> return $ trim out
             _ -> return ""
-    rev <- fmap trim $ readProcess "git" ["rev-parse", "--short", "HEAD"] ""
-    branch <- fmap trim $ readProcess "git" ["rev-parse", "--abbrev-ref", "HEAD"] ""
+    rev <- trim <$> readProcess "git" ["rev-parse", "--short", "HEAD"] ""
+    branch <- trim <$> readProcess "git" ["rev-parse", "--abbrev-ref", "HEAD"] ""
     return (tag, rev, branch)
 
 noVcsInfo :: IO (String, String, String)
